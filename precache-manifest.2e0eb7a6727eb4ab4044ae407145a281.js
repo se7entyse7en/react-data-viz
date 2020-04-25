9bc546f827762c67664975d9a9b2c99ff09f4894
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-25 15:38:05 +0200

    Updates

---
+++ precache-manifest.2e0eb7a6727eb4ab4044ae407145a281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0363bd7f606e5b1575417109f8eb6fd5",
+    "url": "/react-data-viz/index.html"
+  },
+  {
+    "revision": "35767fde79bf4fc4c51e",
+    "url": "/react-data-viz/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "016e8e2b1fd9dfd3ff42",
+    "url": "/react-data-viz/static/js/2.0dc0756e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-data-viz/static/js/2.0dc0756e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35767fde79bf4fc4c51e",
+    "url": "/react-data-viz/static/js/main.b4d97efb.chunk.js"
+  },
+  {
+    "revision": "29dbc1096126ed3cded5",
+    "url": "/react-data-viz/static/js/runtime-main.c33f0876.js"
+  }
+]);
