a870ac4e172d8db8463716924f32ba0b777abb32
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-25 15:32:06 +0200

    Add LineGraph usage in example App
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
 
-import { ExampleComponent } from 'react-data-viz'
-import 'react-data-viz/dist/index.css'
+import { LineGraph } from 'react-data-viz';
+import data from './data';
+import * as d3 from "d3";
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+    const dimensions = {
+        width: window.innerWidth * 0.8,
+        height: 400,
+        margin: {
+            top: 60,
+            right: 15,
+            bottom: 60,
+            left: 70
+        }
+    };
 
-export default App
+    const xAccessor = d => d3.timeParse("%Y-%m-%d")(d.date);
+    const yAccessor = "value";
+
+    const config = {
+        main: {
+            title: "Title",
+            color: "#3498db"
+        },
+        peripherals: {
+            axis: {
+                x: {
+                    title: "x-axis label",
+                    formatTick: d3.timeFormat("%d %b %Y"),
+                    numberOfTicks: 5
+                },
+                y: {
+                    title: "y-axis label",
+                    numberOfTicks: 10
+                }
+            }
+        }
+    };
+
+    return (
+        <>
+        <div style={{margin: '2% 5%', border: 'solid 2px black', textAlign: 'center', fontSize: '28px'}}>React Data Viz</div>
+          <div style={{margin: '2% 5%', border: 'solid 2px black', textAlign: 'center'}}>
+            {data && <LineGraph data={data.timeseries} dimensions={dimensions}
+                                xAccessor={xAccessor} yAccessor={yAccessor}
+                                config={config}></LineGraph>}
+          </div>
+        </>
+    );
+};
+
+export default App;
