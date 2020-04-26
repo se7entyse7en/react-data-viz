4581a48a3be38dc596edc40ddee5f0e2e2ac8774
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-04-26 23:50:26 +0200

    Add example using LineGraphDoubleYAxis
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- example/src/data.js
+++ example/src/data.js
@@ -1,15 +1,15 @@
 const data = {
     timeseries: [
-        {date: "2020-01-01", value: 10},
-        {date: "2020-01-02", value: 20},
-        {date: "2020-01-03", value: 15},
-        {date: "2020-01-04", value: 11},
-        {date: "2020-01-05", value: 100},
-        {date: "2020-01-06", value: 35},
-        {date: "2020-01-07", value: 21},
-        {date: "2020-01-08", value: 83},
-        {date: "2020-01-09", value: 53},
-        {date: "2020-01-10", value: 23},
+        {date: "2020-01-01", value: 10, extraValue: -15},
+        {date: "2020-01-02", value: 20, extraValue: -35},
+        {date: "2020-01-03", value: 15, extraValue: 12},
+        {date: "2020-01-04", value: 11, extraValue: 21},
+        {date: "2020-01-05", value: 100, extraValue: 66},
+        {date: "2020-01-06", value: 35, extraValue: -14},
+        {date: "2020-01-07", value: 21, extraValue: 56},
+        {date: "2020-01-08", value: 83, extraValue: 95},
+        {date: "2020-01-09", value: 53, extraValue: 43},
+        {date: "2020-01-10", value: 23, extraValue: 21},
     ]
 };
 
